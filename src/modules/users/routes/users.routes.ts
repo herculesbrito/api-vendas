989592faa1ce0587693f00d217361242fb9a29d8
hercules brito <herculesbrito@gmail.com>
2022-11-03 18:11:45 -0300

    feat: add avatar route in users rount and static route for avatar

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import UsersAvatarController from '../controllers/UserAvatarController';
 
 const userRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
+
+const upload = multer(uploadConfig);
 
 userRouter.get('/', isAuthenticated, usersController.index);
 
@@ -18,6 +24,13 @@ userRouter.post(
     },
   }),
   usersController.create,
+);
+
+userRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
 );
 
 export default userRouter;
