24c7832e8442cac315d53bf5c1b69c14aaf690a6
hercules brito <herculesbrito@gmail.com>
2022-11-04 10:29:28 -0300

    feat: add user token repository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,27 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const tokenToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return tokenToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const tokenToken = await this.create({
+      user_id,
+    });
+
+    await this.save(tokenToken);
+
+    return tokenToken;
+  }
+}
+
+export default UserTokensRepository;
