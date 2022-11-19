eb6ee013fbd9cfaf9007c730669186e126ff7f7f
hercules brito <herculesbrito@gmail.com>
2022-11-19 10:14:42 -0300

    feat: add orders routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,11 @@
+import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import costumersRouter from '@modules/customers/routes/customers.routes';
-import { Router } from 'express';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 
 const routes = Router();
 
@@ -14,5 +15,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customers', costumersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
