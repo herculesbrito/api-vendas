9c3e1bf86ae41d745664c305a179d42a6f01bd00
hercules brito <herculesbrito@gmail.com>
2022-11-21 19:58:40 -0300

    feat: orders routes add autenticated token

--- src/modules/orders/routes/orders.routes.ts
+++ src/modules/orders/routes/orders.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/OrdersController';
 import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 
 const ordersRouter = Router();
 const ordersController = new OrdersController();
+
+ordersRouter.use(isAuthenticated);
 
 ordersRouter.get(
   '/:id',
