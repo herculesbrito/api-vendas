2b35d8a20aee1757e8f758605592206344249821
hercules brito <herculesbrito@gmail.com>
2022-12-12 17:44:12 -0300

    feat: add infrastructure layer

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import UsersController from '../controllers/UsersController';
+import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
+import UsersAvatarController from '../controllers/UserAvatarController';
+
+const userRouter = Router();
+const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
+
+const upload = multer(uploadConfig.multer);
+
+userRouter.get('/', isAuthenticated, usersController.index);
+
+userRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+userRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
+);
+
+export default userRouter;
---
+++ src/shared/infra/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+import { verify, Secret } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+import { NextFunction, Request, Response } from 'express';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing.');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret as Secret);
+
+    const { sub } = decodedToken as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
